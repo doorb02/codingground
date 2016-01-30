45cf6eb5942ec19a8d5a644ee762a54777284ccb
doorb02 <bert.doornbusch@gmail.com>
2016-01-30 15:24:40 +0100

    Created by Coding Ground Client

---
+++ New Project/Shuttle.java
@@ -0,0 +1,10 @@
+public class Shuttle extends Rocket{
+    public static void main(String[] args) {
+        new Shuttle.go();
+    }
+    void go(){
+        blastOff();
+        // Rocket.blastOff(); // line A
+    }
+    private void blastoff() { System.out.print("sh-bang ")};
+}
